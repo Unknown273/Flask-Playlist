39609b993905f99e4e3fd528d8f25974628320b1
Yusuf Kazi <1485724+Unknown273@users.noreply.github.com>
2023-03-26 11:21:20 +0530

    Completed Video from the Flask playlist

--- server.py
+++ server.py
@@ -6,5 +6,17 @@ app = Flask(__name__)
 def hello():
   return "Hello"
 
+@app.route("/about")
+def about():
+  return "The about page"
+
+@app.route("/blog")
+def blog():
+  return "This is my blog"
+
+@app.route("/blog/<int:blog_id>")
+def blogpost(blog_id):
+  return f"This my blog number {blog_id}"
+
 if __name__ == "__main__":
-  app.run
+  app.run()
